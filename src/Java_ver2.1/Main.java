98d390276a5a062d6d075e577d048c931a0b56b4
Jason834 <72029869+Jason834@users.noreply.github.com>
2020-11-12 21:37:09 +0800

    Update Main.java

--- src/Java_ver2.1/Main.java
+++ src/Java_ver2.1/Main.java
@@ -1,1 +1,32 @@
 
+
+public class Main {
+	
+	public static void main(String[] args)  {
+		
+	
+		
+	
+		String[][] record={{"Bread", "Milk"},
+		                   {"Bread", "Diaper", "Beer", "Eggs"},
+		                   {"Milk", "Diaper", "Beer", "Coke"},
+		                   {"Bread", "Milk", "Diaper", "Beer"},
+		                   {"Bread", "Milk", "Diaper", "Coke"}
+		};
+	
+		String[] user_input1={"0","Milk"};
+		String[] user_input2={"1","Milk"};
+		String[] user_input3={"2","Bread"};
+		RecommandationSystem RS = new RecommandationSystem(record);
+		//RecommandationSystem RS=new RecommandationSystem("record.csv");
+		RS.recommand(user_input1);
+		RS.recommand(user_input2);
+		RS.recommand(user_input3);
+		
+
+		
+	}
+	
+	
+
+}
